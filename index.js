5279d82b02bb1ccf158495e49f82292168d4702e
Star Richardson <67430892+alicenstar@users.noreply.github.com>
2021-05-25 15:52:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const uploadFiles = async () => {
     const auth = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
         .then(response => response.json());
     console.log(`Twilio Signal 2021 ${(new Date()).toTimeString()}`, "date time test");
-    console.log(`${auth.id} ${auth.title`, "access token test");
+    console.log(`${auth.id} ${auth.title}`, "access token test");
     console.log(process.env.SDL_OPTIONS_ID, "options id environment variable test");
 };
 
